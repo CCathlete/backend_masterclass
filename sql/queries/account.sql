6fc25defeb2201b090d34ae458ca194328635f1c
CCat <ccathlete01@gmail.com>
2024-12-11 14:44:09 +0100

    Added update and delete actions.

--- sql/queries/account.sql
+++ sql/queries/account.sql
@@ -16,4 +16,15 @@ WHERE id = $1 limit 1;
 SELECT * FROM accounts
 ORDER BY id
 limit $1
-offset $2;
+offset $2
+;
+
+-- If there are no return values we use :exec instead of :one/many
+-- name: UpdateAccount :one
+UPDATE accounts SET balance = $1 -- , another_param = $3
+where id = $2
+returning *
+;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1;
