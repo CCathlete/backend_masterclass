03ddb3cdf8c75b42ea5baaa71cd6e33f4c44f288
CCat <ccathlete01@gmail.com>
2024-12-11 14:16:49 +0100

    Added list and get (variations of select) actions.

--- sql/queries/account.sql
+++ sql/queries/account.sql
@@ -7,3 +7,13 @@ insert into accounts (
 values ($1, $2, $3)
 returning *
 ;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 limit 1;
+
+-- name: ListAccount :many
+SELECT * FROM accounts
+ORDER BY id
+limit $1
+offset $2;
