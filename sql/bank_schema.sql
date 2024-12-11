334e2a1306c31612c3087655b717c72b8d8241fb
CCat <ccathlete01@gmail.com>
2024-12-11 04:23:50 +0100

    created postgreql code for db creation.

---
+++ sql/bank_schema.sql
@@ -0,0 +1,41 @@
+-- Create the accounts table
+create table
+  accounts (
+    id bigserial primary KEY,
+    followed_user_id integer,
+    owner varchar not null,
+    balance bigint not null, -- Maybe change to decimal later
+    currency varchar not null,
+    created_at timestamptz not null default (now())
+  )
+;
+
+-- Create the entries table
+create table
+  entries (
+    id bigserial primary KEY,
+    account_id bigint references accounts (id),
+    amount bigint, -- Can be positive or negative
+    created_at timestamptz not null default (now())
+  )
+;
+
+-- Create the transfers table
+create table
+  transfers (
+    id bigserial primary KEY,
+    from_account_id bigint references accounts (id),
+    to_account_id bigint references accounts (id),
+    amount bigint check (amount > 0), -- Can be only positive (absolute value)
+    created_at timestamptz not null default (now())
+  )
+;
+
+create index on "accounts" ("owner");
+create index on "entries" ("account_id");
+create index on "transfers" ("from_account_id");
+create index on "transfers" ("to_account_id");
+create index on "transfers" ("from_account_id", "to_account_id");
+
+comment on column "entries"."amount" is 'can be positive or negative';
+comment on column "transfers"."amount" is 'absolute value';
