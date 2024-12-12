3d7c3025dcd0ea985f8af0a981522d58fb5078fc
CCat <ccathlete01@gmail.com>
2024-12-12 13:46:46 +0100

    Wrote tests for delete, get and update account (all passed, test coverage of 20.5%).

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,17 +2,23 @@ package sqlc_test
 
 import (
 	"backend-masterclass/db/sqlc"
+	"backend-masterclass/util"
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+// We pass in the test to run testify/require functions.
+// This function is a validation a preparatino for each
+// test written here.
+func createRandomAccount(t *testing.T) sqlc.Account {
 	arg := sqlc.CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
@@ -25,4 +31,59 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	result, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, result)
+
+	require.Equal(t, account.ID, result.ID)
+	require.Equal(t, account.Owner, result.Owner)
+	require.Equal(t, account.Balance, result.Balance)
+	require.Equal(t, account.Currency, result.Currency)
+	// There might be a short delay from creation of the random account
+	// to its storage in the DB and we don't this to fail the test.
+	require.WithinDuration(t, account.CreatedAt, result.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+
+	arg := sqlc.UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	result, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, result)
+
+	require.Equal(t, account.ID, result.ID)
+	require.Equal(t, account.Owner, result.Owner)
+	// The balance should change to the new value.
+	require.Equal(t, arg.Balance, result.Balance)
+	require.Equal(t, account.Currency, result.Currency)
+	// There might be a short delay from creation of the random account
+	// to its storage in the DB and we don't this to fail the test.
+	require.WithinDuration(t, account.CreatedAt, result.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+
+	// Validation that the account was truly deleted
+	valAccount, err := testQueries.GetAccount(context.Background(), account.ID)
+	// We want an error to occur here.
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, valAccount)
 }
