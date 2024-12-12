23e89129ce95705fbb1a02f4f2dc0a74362a8452
CCat <ccathlete01@gmail.com>
2024-12-12 04:29:07 +0100

    Added connection to db in a separate go file in the db package.

---
+++ db/db_connection.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+// Make sure to db.Close()!
+func ConnectToDB() (*sql.DB, error) {
+	db, err := sql.Open("pgx", connectionString())
+	if err != nil {
+		return nil, fmt.Errorf("error when opening db: %w", err)
+	}
+
+	return db, nil
+}
+
+func connectionString() string {
+	cfg := PostgresConfig{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSL_MODE"),
+	}
+
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User,
+		cfg.Password, cfg.DBName, cfg.SSLMode,
+	)
+}
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	"os"
+	"testing"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+
+	os.Exit(m.Run())
+}
