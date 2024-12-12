2860a1faf8bb11647665c3473250715503800390
CCat <ccathlete01@gmail.com>
2024-12-12 08:34:55 +0100

    Put the test files for sqlc in package sqlc_test (in same sqlc folder, it's common practice).

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -3,7 +3,6 @@ package sqlc_test
 import (
 	"backend-masterclass/db/sqlc"
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 	"testing"
@@ -12,7 +11,7 @@ import (
 var testQueries *sqlc.Queries
 
 func TestMain(m *testing.M) {
-	conn := must(ConnectToDB()).(*sql.DB)
+	conn := must(sqlc.ConnectToDB()).(*sql.DB)
 
 	testQueries = sqlc.New(conn)
 
@@ -25,40 +24,4 @@ func must(value any, err error) any {
 	}
 
 	return value
-}
-
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DBName   string
-	SSLMode  string
-}
-
-// Make sure to db.Close()!
-func ConnectToDB() (*sql.DB, error) {
-	db, err := sql.Open("pgx", connectionString())
-	if err != nil {
-		return nil, fmt.Errorf("error when opening db: %w", err)
-	}
-
-	return db, nil
-}
-
-func connectionString() string {
-	cfg := PostgresConfig{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASS"),
-		DBName:   os.Getenv("DB_NAME"),
-		SSLMode:  os.Getenv("DB_SSL_MODE"),
-	}
-
-	return fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.User,
-		cfg.Password, cfg.DBName, cfg.SSLMode,
-	)
 }
