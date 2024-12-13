6cbe41cb7d6fee0583a89e30d7ba68f134eb4f14
CCat <ccathlete01@gmail.com>
2024-12-13 14:25:39 +0100

    Made an initial prototype for Store.TransferTx.

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -66,3 +66,86 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 
 	return nil
 }
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+// Performs a mony transfer from one account to another.
+// It creates a transfer record, add account entries, and
+// updates accounts' balance within a single DB ransaction.
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx,
+		func(q *Queries) error {
+			var err error
+
+			// The params types have the same fields so we can do a simple
+			// conversion.
+			result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(arg))
+			if err != nil {
+				return fmt.Errorf("TransferTx: %w", err)
+			}
+
+			result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+				AccountID: arg.FromAccountID,
+				Amount:    -(arg.Amount), // Currency exits this account.
+			})
+			if err != nil {
+				return fmt.Errorf("TransferTx: %w", err)
+			}
+
+			result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+				AccountID: arg.ToAccountID,
+				Amount:    +(arg.Amount), // Currency enters this account.
+			})
+			if err != nil {
+				return fmt.Errorf("TransferTx: %w", err)
+			}
+
+			// TODO: needs refactoring.
+			result.ToAccount, err = q.GetAccount(ctx, arg.ToAccountID)
+			if err != nil {
+				return fmt.Errorf("TransferTx: %w", err)
+			}
+
+			result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+				Balance: result.ToAccount.Balance + arg.Amount,
+				ID:      result.ToAccount.ID,
+			})
+			if err != nil {
+				return fmt.Errorf("TransferTx: %w", err)
+			}
+
+			result.FromAccount, err = q.GetAccount(ctx, arg.FromAccountID)
+			if err != nil {
+				return fmt.Errorf("TransferTx: %w", err)
+			}
+
+			result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+				Balance: result.FromAccount.Balance - arg.Amount,
+				ID:      result.FromAccount.ID,
+			})
+			if err != nil {
+				return fmt.Errorf("TransferTx: %w", err)
+			}
+
+			return nil
+		})
+	if err != nil {
+		return TransferTxResult{}, fmt.Errorf("TransferTx: %w", err)
+	}
+
+	return result, nil
+}
