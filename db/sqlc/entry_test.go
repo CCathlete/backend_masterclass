fb22e506bd744e91522b79dec8b2cf9e9297c760
CCat <ccathlete01@gmail.com>
2024-12-13 04:49:33 +0100

    Created all entry tests.

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,106 +1,131 @@
 package sqlc_test
 
-// import (
-// 	"backend-masterclass/db/sqlc"
-// 	"backend-masterclass/util"
-// 	"context"
-// 	"database/sql"
-// 	"testing"
-// 	"time"
+import (
+	"backend-masterclass/db/sqlc"
+	"backend-masterclass/util"
+	"context"
+	"database/sql"
+	"testing"
+	"time"
 
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/stretchr/testify/require"
+)
 
-// // We pass in the test to run testify/require functions.
-// // This function is a validation a preparatino for each
-// // test written here.
-// func createRandomAccount(t *testing.T) sqlc.Account {
-// 	arg := sqlc.CreateAccountParams{
-// 		Owner:    util.RandomOwner(),
-// 		Balance:  util.RandomMoney(),
-// 		Currency: util.RandCurrency(),
-// 	}
+// We pass in the test to run testify/require functions.
+// This function is a validation a preparatino for each
+// test written here.
+func createRandomEntry(t *testing.T) sqlc.Entry {
+	arg := sqlc.CreateEntryParams{
+		AccountID: createRandomAccount(t).ID,
+		Amount:    util.RandomMoney(),
+	}
 
-// 	account, err := testQueries.CreateAccount(context.Background(), arg)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, account)
+	Entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, Entry)
 
-// 	require.Equal(t, arg.Owner, account.Owner)
-// 	require.Equal(t, arg.Balance, account.Balance)
-// 	require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(t, arg.AccountID, Entry.AccountID)
+	require.Equal(t, arg.Amount, Entry.Amount)
 
-// 	require.NotZero(t, account.ID)
-// 	require.NotZero(t, account.CreatedAt)
+	require.NotZero(t, Entry.ID)
+	require.NotZero(t, Entry.CreatedAt)
 
-// 	return account
-// }
+	return Entry
+}
 
-// func TestCreateAccount(t *testing.T) {
-// 	createRandomAccount(t)
-// }
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
 
-// func TestGetAccount(t *testing.T) {
-// 	account := createRandomAccount(t)
-// 	result, err := testQueries.GetAccount(context.Background(), account.ID)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, result)
+func TestGetEntry(t *testing.T) {
+	Entry := createRandomEntry(t)
+	result, err := testQueries.GetEntry(context.Background(), Entry.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, result)
 
-// 	require.Equal(t, account.ID, result.ID)
-// 	require.Equal(t, account.Owner, result.Owner)
-// 	require.Equal(t, account.Balance, result.Balance)
-// 	require.Equal(t, account.Currency, result.Currency)
-// 	// There might be a short delay from creation of the random account
-// 	// to its storage in the DB and we don't this to fail the test.
-// 	require.WithinDuration(t, account.CreatedAt, result.CreatedAt, time.Second)
-// }
+	require.Equal(t, Entry.ID, result.ID)
+	require.Equal(t, Entry.AccountID, result.AccountID)
+	require.Equal(t, Entry.Amount, result.Amount)
+	// There might be a short delay from creation of the random Entry
+	// to its storage in the DB and we don't this to fail the test.
+	require.WithinDuration(t, Entry.CreatedAt, result.CreatedAt, time.Second)
+}
 
-// func TestUpdateAccount(t *testing.T) {
-// 	account := createRandomAccount(t)
+func TestUpdateEntry(t *testing.T) {
+	Entry := createRandomEntry(t)
 
-// 	arg := sqlc.UpdateAccountParams{
-// 		ID:      account.ID,
-// 		Balance: util.RandomMoney(),
-// 	}
+	arg := sqlc.UpdateEntryParams{
+		ID:     Entry.ID,
+		Amount: util.RandomMoney(),
+	}
 
-// 	result, err := testQueries.UpdateAccount(context.Background(), arg)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, result)
+	result, err := testQueries.UpdateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, result)
 
-// 	require.Equal(t, account.ID, result.ID)
-// 	require.Equal(t, account.Owner, result.Owner)
-// 	// The balance should change to the new value.
-// 	require.Equal(t, arg.Balance, result.Balance)
-// 	require.Equal(t, account.Currency, result.Currency)
-// 	// There might be a short delay from creation of the random account
-// 	// to its storage in the DB and we don't this to fail the test.
-// 	require.WithinDuration(t, account.CreatedAt, result.CreatedAt, time.Second)
-// }
+	require.Equal(t, Entry.ID, result.ID)
+	require.Equal(t, Entry.AccountID, result.AccountID)
+	// The balance should change to the new value.
+	require.Equal(t, arg.Amount, result.Amount)
+	// There might be a short delay from creation of the random Entry
+	// to its storage in the DB and we don't this to fail the test.
+	require.WithinDuration(t, Entry.CreatedAt, result.CreatedAt, time.Second)
+}
 
-// func TestDeleteAccount(t *testing.T) {
-// 	account := createRandomAccount(t)
-// 	err := testQueries.DeleteAccount(context.Background(), account.ID)
-// 	require.NoError(t, err)
+func TestUpdateEntryByAccount(t *testing.T) {
+	account := createRandomAccount(t)
 
-// 	// Validation that the account was truly deleted
-// 	valAccount, err := testQueries.GetAccount(context.Background(), account.ID)
-// 	// We want an error to occur here.
-// 	require.EqualError(t, err, sql.ErrNoRows.Error())
-// 	require.Empty(t, valAccount)
-// }
+	createArg := sqlc.CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
 
-// func TestListAccounts(t *testing.T) {
-// 	for i := 0; i < 10; i++ {
-// 		createRandomAccount(t)
-// 	}
+	entry, err := testQueries.CreateEntry(context.Background(), createArg)
+	require.NoError(t, err)
 
-// 	arg := sqlc.ListAccountParams{
-// 		Limit:  5,
-// 		Offset: 5, // Skips 5 matches before returning values.
-// 	}
-// 	results, err := testQueries.ListAccount(context.Background(), arg)
-// 	require.NoError(t, err)
+	updateArg := sqlc.UpdateEntryByAccountParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
 
-// 	for _, result := range results {
-// 		require.NotEmpty(t, result)
-// 	}
-// }
+	result, err := testQueries.UpdateEntryByAccount(context.Background(), updateArg)
+	require.NoError(t, err)
+	require.NotEmpty(t, result)
+
+	require.Equal(t, entry.ID, result.ID)
+	require.Equal(t, entry.AccountID, result.AccountID)
+	// The balance should change to the new value.
+	require.Equal(t, updateArg.Amount, result.Amount)
+	// There might be a short delay from creation of the random entry
+	// to its storage in the DB and we don't this to fail the test.
+	require.WithinDuration(t, entry.CreatedAt, result.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	Entry := createRandomEntry(t)
+	err := testQueries.DeleteEntry(context.Background(), Entry.ID)
+	require.NoError(t, err)
+
+	// Validation that the Entry was truly deleted
+	valEntry, err := testQueries.GetEntry(context.Background(), Entry.ID)
+	// We want an error to occur here.
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, valEntry)
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t)
+	}
+
+	arg := sqlc.ListEntriesParams{
+		Limit:  5,
+		Offset: 5, // Skips 5 matches before returning values.
+	}
+	results, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+
+	for _, result := range results {
+		require.NotEmpty(t, result)
+	}
+}
