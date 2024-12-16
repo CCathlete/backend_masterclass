3b00a4ec3cc7296dc9090b9f19d71f10cee075e2
CCat <ccathlete01@gmail.com>
2024-12-16 16:26:25 +0100

    Created a cmd/main.go that starts a server + a createAccount handler.

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"backend-masterclass/api"
 	"backend-masterclass/db/sqlc"
 	"backend-masterclass/util"
 	"database/sql"
 )
 
+const (
+	serverAddress = "0.0.0.0:8080"
+)
+
 func main() {
 	conn := util.Must(sqlc.ConnectToDB()).(*sql.DB)
 	defer conn.Close()
+
+	store := sqlc.NewStore(conn)
+	server := api.NewServer(store)
+
+	util.Must(nil, server.Start(serverAddress))
 }
