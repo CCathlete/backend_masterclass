6ebd32f0337101d214714462042d68e8ac069cdf
CCat <ccathlete01@gmail.com>
2024-12-17 04:39:16 +0100

    Finished the list account handler.

--- api/account.go
+++ api/account.go
@@ -59,8 +59,15 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, account)
 }
 
+/*
+We want to display the list of accounts in chunks (pages). Each chunk
+has a size of page_size. In order to navigate to the right place
+in the whole list, we need to know how many pages to skip, this is
+the offset which is the (num_of_pages_to_skip - 1) * page_size.
+*/
 type listAccountsRequest struct {
-	ID int64 `uri:"id" binding:"required,min=1"`
+	pageID   int32 `form:"page_id" binding:"required,min=1"`
+	pageSize int32 `form:"page_size" binding:"required,min=5,max=5"`
 }
 
 func (server *Server) listAccounts(ctx *gin.Context) {
@@ -71,16 +78,12 @@ func (server *Server) listAccounts(ctx *gin.Context) {
 	}
 
 	arg := sqlc.ListAccountParams{
-		Limit:  5,
-		Offset: 1,
+		Limit:  req.pageSize,
+		Offset: (req.pageID - 1) * req.pageSize,
 	}
 
 	account, err := server.store.ListAccount(ctx, arg)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return
-		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
