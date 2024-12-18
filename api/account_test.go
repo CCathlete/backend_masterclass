e0c6d5bb2873ee50d8284f42968a279fbf282438
CCat <ccathlete01@gmail.com>
2024-12-18 14:48:34 +0100

    Started creating a test for api using mockStore.

--- api/account_test.go
+++ api/account_test.go
@@ -1,13 +1,50 @@
 package api_test
 
 import (
+	"backend-masterclass/api"
+	mockdb "backend-masterclass/db/mock"
 	"backend-masterclass/db/sqlc"
 	u "backend-masterclass/util"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func TestGetAccountAPI(t *testing.T) {
+	account := randomAccount()
 
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+	// build stubs.
+	store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+		Times(1).
+		Return(account, nil)
+
+	// Starting test server and sending request.
+	server := api.NewServer(store)
+	recorder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/accounts/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	// We use the inner ServeHTTP method and not Gin's Run method because
+	// we want to use our recorder as a response writer.
+	server.Router.ServeHTTP(recorder, request)
+	// Checking response.
+	require.Equal(t, http.StatusOK, recorder.Code)
+
+	// Check response's body.
+	requireMatchAccount(t, account, recorder.Body)
 }
 
 func randomAccount() sqlc.Account {
@@ -17,4 +54,17 @@ func randomAccount() sqlc.Account {
 		Balance:  u.RandomMoney(),
 		Currency: u.RandCurrency(),
 	}
+}
+
+func requireMatchAccount(t *testing.T,
+	account sqlc.Account,
+	body *bytes.Buffer,
+) {
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccount sqlc.Account
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, account, gotAccount)
 }
--- api/server.go
+++ api/server.go
@@ -10,29 +10,29 @@ import (
 
 type Server struct {
 	store  sqlc.Store
-	router *gin.Engine
+	Router *gin.Engine
 }
 
 func NewServer(store sqlc.Store) (s *Server) {
 	s = &Server{
 		store:  store,
-		router: gin.Default(),
+		Router: gin.Default(),
 	}
 	// POST requests:
-	s.router.POST("/accounts", s.createAccount)
-	s.router.POST("/accounts/updbalance", s.updateAccountBalance)
-	s.router.POST("/accounts/setbalance", s.updateAccount)
-	// GET requests:
-	s.router.GET("/accounts/", s.listAccounts)
-	s.router.GET("/accounts/:id", s.getAccount)
-	s.router.GET("/accounts/forupdate/:id", s.getAccountForUpdate)
-	s.router.GET("/accounts/delete/:id", s.deleteAccount)
+	s.Router.POST("/accounts", s.createAccount)
+	s.Router.POST("/accounts/updbalance", s.updateAccountBalance)
+	s.Router.POST("/accounts/setbalance", s.updateAccount)
+	// GET Requests:
+	s.Router.GET("/accounts/", s.listAccounts)
+	s.Router.GET("/accounts/:id", s.getAccount)
+	s.Router.GET("/accounts/forupdate/:id", s.getAccountForUpdate)
+	s.Router.GET("/accounts/delete/:id", s.deleteAccount)
 
 	return
 }
 
 func (server *Server) Start(address string) (err error) {
-	return server.router.Run(address)
+	return server.Router.Run(address)
 }
 
 func errorResponse(err error) (resBody gin.H) {
