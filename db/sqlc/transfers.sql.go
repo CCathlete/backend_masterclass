82d50d375e53ce30a76350732990bc544e4ecfff
CCat <ccathlete01@gmail.com>
2024-12-20 10:15:33 +0100

    Added the currency field to entry and transfer and regenerated qith sqlc

--- db/sqlc/transfers.sql.go
+++ db/sqlc/transfers.sql.go
@@ -11,20 +11,26 @@ import (
 
 const createTransfer = `-- name: CreateTransfer :one
 insert into
-  transfers (from_account_id, to_account_id, amount)
+  transfers (from_account_id, to_account_id, amount, currency)
 values
-  ($1, $2, $3)
+  ($1, $2, $3, $4)
   returning id, from_account_id, to_account_id, amount, currency, created_at
 `
 
 type CreateTransferParams struct {
-	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:"to_account_id"`
-	Amount        int64 `json:"amount"`
+	FromAccountID int64  `json:"from_account_id"`
+	ToAccountID   int64  `json:"to_account_id"`
+	Amount        int64  `json:"amount"`
+	Currency      string `json:"currency"`
 }
 
 func (q *Queries) CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfer, error) {
-	row := q.db.QueryRowContext(ctx, createTransfer, arg.FromAccountID, arg.ToAccountID, arg.Amount)
+	row := q.db.QueryRowContext(ctx, createTransfer,
+		arg.FromAccountID,
+		arg.ToAccountID,
+		arg.Amount,
+		arg.Currency,
+	)
 	var i Transfer
 	err := row.Scan(
 		&i.ID,
