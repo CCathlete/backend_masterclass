19d20573ccc4968cc620b6a396be836cab62156b
CCat <ccathlete01@gmail.com>
2024-12-21 15:57:26 +0100

    Created a TestPassword function.

--- util/password_test.go
+++ util/password_test.go
@@ -9,13 +9,27 @@ import (
 
 func TestPassword(t *testing.T) {
 	password := RandomPassword()
-	hash, err := HashPassword(password)
+
+	hash1, err := HashPassword(password)
 	require.NoError(t, err)
 
-	err = CheckPassword(password, hash)
+	err = CheckPassword(password, hash1)
 	require.NoError(t, err)
+	require.NotEmpty(t, hash1)
 
+	// Compare the hash with a wrong password's hash.
 	wrongPassword := RandomPassword()
-	err = CheckPassword(wrongPassword, hash)
+	err = CheckPassword(wrongPassword, hash1)
 	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	// Now we'll hash the password again (will provide a different hash becaue of the salt) and check the password with the new hash.
+	hash2, err := HashPassword(password)
+	require.NoError(t, err)
+
+	err = CheckPassword(password, hash2)
+	require.NoError(t, err)
+	require.NotEmpty(t, hash2)
+
+	// Compare the two hashes.
+	require.NotEqual(t, hash1, hash2)
 }
