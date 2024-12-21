440b6f1b02a1c582e97798f8df0f9813bb7c9875
CCat <ccathlete01@gmail.com>
2024-12-21 15:49:02 +0100

    Created a TestPassword function.

---
+++ util/password_test.go
@@ -0,0 +1,21 @@
+package u
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomPassword()
+	hash, err := HashPassword(password)
+	require.NoError(t, err)
+
+	err = CheckPassword(password, hash)
+	require.NoError(t, err)
+
+	wrongPassword := RandomPassword()
+	err = CheckPassword(wrongPassword, hash)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
